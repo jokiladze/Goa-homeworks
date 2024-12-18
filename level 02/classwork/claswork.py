67f9bee22b6d6d7bda699873586d88d96463cef8
jokiladze <rezi99742@gmail.com>
2024-12-18 15:06:21 +0300

    second commit

---
+++ level 02/classwork/claswork.py
@@ -0,0 +1,15 @@
+#საკლასო დავალება 
+# 1.
+# print გამოყენებით დაბეჭდეთ (სახელი,გვარი,ასაკი)
+print('rezi')
+print('jokiladze')
+print(13)
+
+
+
+# 2. ცვლადის დახმარებით შემოიტანეთ თქვენი (სახელი,გვარი,ასაკი,hobby,საყვარელი მანქანა და დაპრინტეთ ერთ ხაზზე)
+name = 'rezi'
+last_name = 'jokiladze'
+hob = 'train'
+faworite_car = 'bmw'
+print(name + " " + last_name + ' ' + hob + ' ' + faworite_car)
