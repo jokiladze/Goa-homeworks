3214a5c045ed56823f714f4c767193723724897b
jokiladze <rezi99742@gmail.com>
2025-02-13 15:53:14 +0300

    secon commit

---
+++ level 17/claswork/claswork.py
@@ -0,0 +1,20 @@
+#1) for loop-ში გამოიტანეთ 950-დან 1000-ის ჩათვლით ლუწი რიცხვები              
+for i in range(950, 1001, 2):
+    print(i)
+
+#2) for loop-ში გამოიტანეთ თქვენი სახელი 100-ჯერ 
+
+for i in range(100):
+    print("rezi")  
+
+#3) for loop-ში გამოიტანე "Goa Is the best" 1000-ჯერ
+
+2
+for i in range(1000):
+    print("Goa Is the best")
+
+
+#4) for loop-ში გამოიტანე რიცხვები 100-დან 200-მდე
+
+for i in range(100, 201):
+    print(i)
